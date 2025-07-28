56a5c377486ea5c74d5b8c7ecdf3e1457e5d0942
unborntadpole <unborntadpole@gmail.com>
2025-07-29 00:14:06 +0530

    Switched from PyTorch to scikit-learn for model handling and removed unused imports.

--- app/main.py
+++ app/main.py
@@ -1,7 +1,11 @@
 from fastapi import FastAPI, CORSMiddleware
 from pydantic import BaseModel
-import torch
-from app.model import LogisticRegression
+import pickle
+import numpy as np
+
+
+with open('model.pkl', "rb") as f:
+    model = pickle.load(f)
 
 app = FastAPI()
 
@@ -13,30 +17,15 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
-input_dim = 4
-output_dim = 3
-model = LogisticRegression(input_dim, output_dim)
-model.load_state_dict(torch.load("app/model.pt"))
-model.eval()
+class InputData(BaseModel):
+    features: list[float]
 
-
-class IrisInput(BaseModel):
-    sepal_length: float
-    sepal_width: float
-    petal_length: float
-    petal_width: float
-
+@app.get("/")
+def root():
+    return {"message": "Iris classifier is live!"}
 
 @app.post("/predict")
-def predict(input: IrisInput):
-    features = [[
-        input.sepal_length,
-        input.sepal_width,
-        input.petal_length,
-        input.petal_width
-    ]]
-    tensor = torch.tensor(features, dtype=torch.float32)
-    with torch.no_grad():
-        logits = model(tensor)
-        predicted = torch.argmax(logits, dim=1).item()
-    return {"predicted_class": predicted}
+def predict(data: InputData):
+    features = np.array(data.features).reshape(1, -1)
+    prediction = model.predict(features)[0]
+    return {"prediction": int(prediction)}
--- app/model.py
+++
@@ -1,9 +0,0 @@
-import torch.nn as nn
-
-class LogisticRegression(nn.Module):
-    def __init__(self, input_dim, output_dim):
-        super().__init__()
-        self.linear = nn.Linear(input_dim, output_dim)
-
-    def forward(self, x):
-        return self.linear(x)
